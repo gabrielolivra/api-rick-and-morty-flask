5a4fcf2af2db02a1af944ec3e46f0c2463d2c049
GB <89982150+gabrielolivra@users.noreply.github.com>
2024-03-22 09:45:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     pagina = request.args.get('pagina', default=1, type=int)
+    if pagina <= 0:
+        pagina = 1
+    
     api = requests.get(f'https://rickandmortyapi.com/api/character?page={pagina}')
     personagens = api.json()
     return render_template("home.html", personagem=personagens['results'], pagina_atual=pagina)
@@ -15,7 +18,6 @@ def enviar(personagem):
     api = requests.get(f'https://rickandmortyapi.com/api/character/{personagem}')
     personagem_id = api.json()
     return render_template("info.html", personagem_id = personagem_id)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
